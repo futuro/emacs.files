d7e7c6b7bf5c9a6a9fe75b0454b5aa61a6f0e932
Evan Niessen-Derry <eniessenderry@gmail.com>
2019-01-22 23:50:36 -0600

    building up to something

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;;;
+;;  Some hand-coded preferred options
+;;;
+
 ;; delete excess backup versions silently
 (setq delete-old-versions -1)
 ;; use version control
@@ -32,3 +36,39 @@
 
 ;; print a default message in the empty scratch buffer opened at startup
 (setq initial-scratch-message "Welcome to Emacs")
+
+;;;
+;;  And now some packages
+;;;
+
+(require 'package)
+;; tells emacs not to load any packages before starting up
+(setq package-enable-at-startup nil)
+;; the following lines tell emacs where on the internet to look up
+;; for new packages.
+(setq package-archives '(("org"       . "https://orgmode.org/elpa/")
+                         ("gnu"       . "https://elpa.gnu.org/packages/")
+                         ("melpa"     . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package) ; unless it is already installed
+  (package-refresh-contents) ; update packages archive
+  (package-install 'use-package)) ; and install the most recent version of use-package
+
+(require 'use-package)
+
+;; A nicer way to define key bindings
+;; https://github.com/noctuid/general.el#about
+(use-package general :ensure t
+	     :config
+	     (general-define-key "C-'" 'avy-goto-word-1))
+
+;; "avy is a GNU Emacs package for jumping to visible text
+;;  using a char-based decision tree."
+;; https://github.com/abo-abo/avy#introduction
+(use-package avy :ensure t
+	     :commands (avy-goto-word-1))
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
