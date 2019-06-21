56268283dfc3ad8ccb9c1f5f5f3240c08ecaac1f
Evan Niessen-Derry <evan.niessen-derry@healthfinch.com>
2019-06-21 10:13:49 -0500

    Record customization changes
    Though, now that I think about it, this may be specific to a machine,
    and should be changed in the init file. Hmm...

--- custom.el
+++ custom.el
@@ -3,10 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key counsel ivy avy general use-package))))
+ '(face-font-family-alternatives
+   (quote
+    (("Monospace" "Source Code Pro for Powerline" "courier" "fixed")
+     ("Monospace Serif" "Courier 10 Pitch" "Consolas" "Courier Std" "FreeMono" "Nimbus Mono L" "courier" "fixed")
+     ("courier" "CMU Typewriter Text" "fixed")
+     ("Sans Serif" "helv" "helvetica" "arial" "fixed")
+     ("helv" "helvetica" "arial" "fixed"))))
+ '(package-selected-packages
+   (quote
+    (rainbow-delimiters magit auto-package-update which-key counsel ivy avy general use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Source Code Pro for Powerline")))))
