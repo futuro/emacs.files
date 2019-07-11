4c5c83abd94ecb713e7b13e1afcbb9df1ff3a61e
Evan Niessen-Derry <evan.niessen-derry@healthfinch.com>
2019-07-11 10:19:55 -0500

    Commit some line number customizations

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(display-line-numbers-grow-only t)
+ '(display-line-numbers-type t)
  '(face-font-family-alternatives
    (quote
     (("Monospace" "Source Code Pro for Powerline" "courier" "fixed")
