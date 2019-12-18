d9242e7aac78eaeea7590d006a5a64f61218e3f6
Evan Niessen-Derry <evan.niessen-derry@healthfinch.com>
2019-12-18 09:34:16 -0600

    Configure nicer code highlighting in org exports

--- init.el
+++ init.el
@@ -263,6 +263,25 @@
   :bind
   ("C-c l" . org-store-link)
   ("C-c c" . org-capture))
+(use-package htmlize)
+
+;; Jacked from https://emacs.stackexchange.com/q/3374
+(defun my/org-inline-css-hook (exporter)
+  "Set code block backgrounds to current theme background.
+
+For the HTML EXPORTER, insert custom inline css to
+automatically set the background of code blocks to the current
+theme's background."
+  (when (eq exporter 'html)
+    (let* ((my-pre-bg (face-background 'default))
+           (my-pre-fg (face-foreground 'default)))
+      (setq
+       org-html-head-extra
+       (concat
+        org-html-head-extra
+        (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
+                my-pre-bg my-pre-fg))))))
+
 
 (use-package csv-mode)
 
