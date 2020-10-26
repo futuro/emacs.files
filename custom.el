2ff9ea2ef5aca5d9349b6a2c49108e52cb0fbd2f
Evan Niessen-Derry <eniessenderry@gmail.com>
2020-10-26 05:59:04 -0500

    Commit some auto-changes to custom.el
    Specifically how it's switches to syntax-quote instead of
    function-quote.

--- custom.el
+++ custom.el
@@ -4,28 +4,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("7aaee3a00f6eb16836f5b28bdccde9e1079654060d26ce4b8f49b56689c51904" default)))
+   '("7aaee3a00f6eb16836f5b28bdccde9e1079654060d26ce4b8f49b56689c51904" default))
  '(package-selected-packages
    (quote
     (wgrep edit-indirect ace-window forge htmlize htlmize flycheck robe csv-mode ivy-hydra org-plus-contrib git-link company flx counsel-projectile projectile rainbow-delimiters magit auto-package-update which-key counsel ivy avy general use-package)))
  '(safe-local-variable-values
-   (quote
-    ((put-clojure-indent
-      (quote action)
-      :defn)
-     (eval put-clojure-indent
-	   (quote optional-field)
-	   (quote defun))
-     (eval put-clojure-indent
-	   (quote field)
-	   (quote defun))
-     (eval put-clojure-indent
-	   (quote variant)
-	   (quote defun))
-     (eval put-clojure-indent
-	   (quote alias)
-	   1)))))
+   '((put-clojure-indent 'action :defn)
+     (eval put-clojure-indent 'optional-field 'defun)
+     (eval put-clojure-indent 'field 'defun)
+     (eval put-clojure-indent 'variant 'defun)
+     (eval put-clojure-indent 'alias 1))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
